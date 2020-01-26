16884b15db99ec253b2340c616fd69b6a570857f
Loic <loicvieu@outlook.fr>
2020-01-26 17:23:51 +0100

    création des lignes 1 et 2

--- Classe.py
+++ Classe.py
@@ -4,9 +4,10 @@ Created on Wed Jan 22 09:00:51 2020
 
 @author: vieul
 """
+import datetime
 
 class Arret:
-    def __init__(self, nom_arret, heure_normal_go = [], heure_wk_go = [], heure_normal_back = [], heure_wk_back = [], precedant = [], suivant = [], ligne = str()):
+    def __init__(self, nom_arret, heure_normal_go = [], heure_wk_go = [], heure_normal_back = [], heure_wk_back = [], precedant = [], suivant = [], ligne = int()):
         self.nom_arret = nom_arret
         self.heure_normal_go = heure_normal_go
         self.heure_wk_go = heure_wk_go
@@ -14,6 +15,7 @@ class Arret:
         self.heure_wk_back = heure_wk_back      
         self.suivant = suivant
         self.ligne = ligne
+        self.precedant = precedant
 
 # =============================================================================
 # Création des set et des get        
@@ -33,18 +35,68 @@ class Arret:
     def set_heure_wk_back(self,l):
         self.heure_wk_back = l
         
-    def set_suivant(self,liste):
-        self.suivant = liste
-        
+    def set_suivant(self,l):
+        self.suivant = l
+    
+    def set_precedant(self,l):
+        self.precedant = l
+    
     def set_ligne(self,l):
         self.ligne = l
         
         
     def get_nom(self):
         return self.nom_arret
+# =============================================================================
+# Création des primitives    
+# =============================================================================
+    def premier_bus(self,he,dest,h):
+        '''
+        Cette fonction prend comme paramètre
+         - une heure(str) au format HH:MM
+         - une destination: Back ou Go
+         - une periode: normal ou wk
+        '''
+        # dest = input('Go: g ou Black : b    :')
+        # h = input('Normal: n ou WK: w       :')
+        heure = datetime.datetime.strptime(he, '%H:%M')
+
+        if dest == 'g' and h == 'n':
+            for i in range(len(self.heure_normal_go)):
+                if self.heure_normal_go[i] != '-' and  heure < self.heure_normal_go[i]:               
+                    return [self.heure_normal_go[i],i]
+        
+        elif dest == 'g' and h == 'w':
+            for i in range(len(self.heure_wk_go)):
+                if self.heure_wk_go[i] != '-' and heure < self.heure_wk_go[i]:
+                    return [self.heure_wk_go[i],i]
+        
+        elif dest == 'b' and h == 'n' :
+            for i in range(len(self.heure_normal_back)):
+                if self.heure_normal_back[i] != '-' and heure < self.heure_normal_back[i]:
+                    return [self.heure_normal_back[i],i]
+        
+        elif dest == 'b' and h == 'w' :
+            for i in range(len(self.heure_wk_back)):
+                if self.heure_wk_back[i] != '-' and heure < self.heure_wk_back[i]:
+                    return [self.heure_wk_back[i],i]
+        else:
+            print ('Erreur')
     
-    
-    
+    def difference(self,arret,i,s,h):
+        '''
+        Cette fonction renvoi le temps d'att entre deux arrets pour un bus donnée
+        '''
+        if s == 'g' and h == 'n' :
+            return  arret.heure_normal_go[i] - self.heure_normal_go[i]
+        elif s == 'g' and h == 'w' :
+            return arret.heure_wk_go[i] - self.heure_wk_go[i]
+        elif s == 'b' and h == 'n' :
+            return arret.heure_normal_back[i] - self.heure_normal_back[i]
+        elif s =='b' and h == 'w' :
+            return arret.heure_wk_back[i] - self.heure_wk_back[i] 
+        
+        
 class Ligne:
     def __init__(self ,nom = int(), arrets = [], correspondance = []):
         self.nom = nom
@@ -62,7 +114,16 @@ class Ligne:
         
     def get_arrets(self):
         return self.arrets
+    
+    
+    
+class Voyage:
+    def __init__(self ,destination, depart, moment = str()):
+        self.destination = destination
+        self.depart = depart
+        self.moment = moment
         
+    
         
         
 
--- Main.py
+++ Main.py
@@ -5,14 +5,15 @@ Created on Wed Jan 22 09:00:36 2020
 @author: vieul
 """
 from Classe import *
+import datetime
 
 # =============================================================================
 # Importation des données
 # =============================================================================
 
 # Importation des données
-data_file_name = 'E://0_travail//Proj631_Sibra//data//1_Poisy-ParcDesGlaisins.txt'
-data_file_name2 = 'E://0_travail//Proj631_Sibra//data//2_Piscine-Patinoire_Campus.txt'
+data_file_name = '1_Poisy-ParcDesGlaisins.txt'
+data_file_name2 = '2_Piscine-Patinoire_Campus.txt'
 
 try:
     with open(data_file_name, 'r',encoding = 'utf-8') as f:
@@ -75,49 +76,64 @@ def ajout_arrets_ligne(path,ligne):
 #        path[u] = path[u].lower()
 #    # Ajout des arrets dans le ligne 1
     ligne.set_arrets(path)  
-
-        
-def création_var_arret(ligne,pre,date_n_go,date_wk_go):
-    """
-    Crée des variables de classe Arret et ajoute le nom de l'arret
+def transforme_en_heur(liste):
+    '''
+    Cette fonction prends en paramètre une liste comportant des str aux format HH:MM ou des '-'
+    elle retourne une liste avec d'heur de type heur
+    !! La package datetime est obligatoire !!
     
+    '''
+    a = []
+    
+    for u in liste:
+        if u != '-':
+            a.append(datetime.datetime.strptime(u, '%H:%M'))
+        else: 
+            a.append(u)
+    return a
+        
+def creation_var_arret(ligne,pre,date_n_go,date_wk_go,date_n_back,date_wk_back,nligne):
+    """
+    Cette fonction a pour objectif de crée les variables et les constructeurs de la classe
+    Cette fonction prends pour paramètre un objet de type ligne,
+    l'ensemble des horraires et le numéro de la ligne
     """
     for i in range(len(ligne.arrets)):
+#        datetime.datetime.strptime(date_n_go[ligne.arrets[i]], '%H:%M')
         globals()[str(pre) + str(i)] = Arret(ligne.arrets[i])
-        globals()[str(pre) + str(i)].set_heure_normal_go(date_n_go[ligne.arrets[i]])
-        globals()[str(pre) + str(i)].set_heure_wk_go(date_wk_go[ligne.arrets[i]])
-
-        
-
-
-        
-
+        globals()[str(pre) + str(i)].set_heure_normal_go(transforme_en_heur(date_n_go[ligne.arrets[i]]))
+        globals()[str(pre) + str(i)].set_heure_wk_go(transforme_en_heur(date_wk_go[ligne.arrets[i]]))
+        globals()[str(pre) + str(i)].set_heure_normal_back(transforme_en_heur(date_n_back[ligne.arrets[i]]))
+        globals()[str(pre) + str(i)].set_heure_wk_back(transforme_en_heur(date_wk_back[ligne.arrets[i]]))
+        if i != len(ligne.arrets)-1:  
+            globals()[str(pre) + str(i)].set_suivant([ligne.arrets[i+1]])
+        if i != 0:
+            globals()[str(pre) + str(i)].set_precedant([ligne.arrets[i-1]])
+        globals()[str(pre) + str(i)].set_ligne(nligne)
     
+# =============================================================================
+# Programme Principal
+# =============================================================================
 
+# Création des lignes de bus
 ajout_arrets_ligne(regular_path,ligne1)
 ajout_arrets_ligne(regular_path2,ligne2)
-l1 = création_var_arret(ligne1,'varl1_',regular_date_go,we_holidays_date_go)
-l2 = création_var_arret(ligne2,'varl2_',regular_date_go2,we_holidays_date_go2)
+
+#Création d'une variable pour chaque arrets
+creation_var_arret(ligne1,'varl1_',regular_date_go,we_holidays_date_go,regular_date_back,we_holidays_date_back,1)
+creation_var_arret(ligne2,'varl2_',regular_date_go2,we_holidays_date_go2,regular_date_back2,we_holidays_date_back2,2)
 
 
 
+# =============================================================================
+# Test console
+# =============================================================================
 
+#print(varl1_1.nom_arret,varl1_1.heure_normal_go, '\n\n',varl1_1.heure_wk_go,'\n\n',varl1_1.heure_normal_back,'\n\n',varl1_1.heure_wk_back)
+#print(varl1_0.suivant,varl1_0.precedant,varl1_0.ligne)
+#print(ligne1.arrets)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(varl1_1.premier_bus('8:22','g','n'))
 
 
 
